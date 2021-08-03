bf3693b6bafa4f63f650ed3e0d7da1eb81450ec6
Rodrigo Lima <limarodrigoo@outlook.com>
2021-08-03 16:07:14 -0300

    Requisito 07

--- src/challenges.js
+++ src/challenges.js
@@ -1,6 +1,6 @@
 // Desafio 1
 function compareTrue(a, b) {
-  if (a === true && b === true){
+  if (a === true && b === true) {
     return true;
   } else {
     return false;
@@ -10,7 +10,7 @@ function compareTrue(a, b) {
 // Desafio 2
 function calcArea(base, height) {
   // seu código aqui
-  return (base * height)/2;
+  return (base * height) / 2;
 }
 
 // Desafio 3
@@ -28,7 +28,7 @@ function concatName(arr) {
 // Desafio 5
 function footballPoints(wins, ties) {
   // seu código aqui
-  return (wins * 3) + (ties); 
+  return (wins * 3) + (ties);
 }
 
 // Desafio 6
@@ -36,29 +36,44 @@ function highestCount(arr) {
   // seu código aqui
   let highestNum = arr[0];
   let highestNumCont = 0;
-  for( let key in arr ){
-    if( arr[key] > highestNum ){
+  for (let key in arr) {
+    if (arr[key] > highestNum) {
       highestNum = arr[key];
     }
   }
-  for( let key2 in arr ){
-    if( arr[key2] === highestNum ){
+  for (let key2 in arr) {
+    if (arr[key2] === highestNum) {
       highestNumCont += 1;
     }
-  } 
-
-  console.log(highestNum);
-  console.log(highestNumCont);
+  }
   return highestNumCont;
 }
 
 highestCount([-2, -2, -1]);
 
 // Desafio 7
-function catAndMouse() {
+function catAndMouse(mouse, cat1, cat2) {
   // seu código aqui
+  let distance1 = 0;
+  let distance2 = 0;
+
+  distance1 = Math.abs(cat1 - mouse);
+  distance2 = Math.abs(cat2 - mouse);
+
+  console.log(distance1,distance2);
+
+  if (distance1 < distance2) {
+    return 'cat1';
+  } else if (distance2 < distance1) {
+    return 'cat2';
+  }  else if( distance1 === distance2) {
+    return 'os gatos trombam e o rato foge';
+  }
+
 }
 
+catAndMouse(1, 0, 2);
+console.log(catAndMouse(1, 0, 2));
 // Desafio 8
 function fizzBuzz() {
   // seu código aqui
